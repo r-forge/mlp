9c42bcd70ac839c2cfd69cc4e6672e66dacb377f
tobias <unknown@noreply.com>
2009-09-19 09:32:48 +0000

    first reading of the code; replaced f.cmeans by colMeans and f.rmeans by rowMeans; changed all indicator variables to logicals (TRUE/FALSE); replaced f.Vec2string by paste(., collapse = ""); miscellaneous formatting corrections; added DESCRIPTION
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mlp@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MLPSetUp.R
+++ pkg/R/MLPSetUp.R
@@ -8,29 +8,31 @@
 ###Required Libraries:
 
 ###Organism library:
-library(affy)
-library(org.Hs.eg.db)   ### library(org.Mm.eg.db)  ### library(org.Rn.eg.db)
-library(GO.db)
-library(affy)
-library(AnnotationDbi)
+#library(affy)
+#library(org.Hs.eg.db)   ### library(org.Mm.eg.db)  ### library(org.Rn.eg.db)
+#library(GO.db)
+#library(affy)
+#library(AnnotationDbi)
 
-
-
-f.GOAnnotation <- function(Org="Mouse", Onto="BP",fNames=fNames,nMin=5,nMax=100){
-
-###Authors: An De Bondt, Tine Casneuf, Nandini Raghavan.
-###This code creates the necessary inputs for MLP analysis using libraries available from Bioconductor.
-
-###Inputs: 
-###Org=c("Mouse",Human","Rat")
-###Onto=c("MF",BP","CC")
-###fNames = feature names in the dataset (Entrez IDs).
-###nMin = minimum number of genes in geneset.
-###nMax = maximum number of genes in geneset.
-
-  if(Org=="Mouse"){library(org.Mm.eg.db);go2entrez <- as.list(org.Mm.egGO2ALLEGS)}
-  if(Org=="Human"){library(org.Hs.eg.db);go2entrez <- as.list(org.Hs.egGO2ALLEGS)}
-  if(Org=="Rat"){library(org.Rn.eg.db);  go2entrez <- as.list(org.Rn.egGO2ALLEGS)}
+f.GOAnnotation <- function(Org="Mouse", Onto="BP", fNames=fNames, nMin=5,nMax=100){
+  
+  ###Authors: An De Bondt, Tine Casneuf, Nandini Raghavan.
+  ###This code creates the necessary inputs for MLP analysis using libraries available from Bioconductor.
+  
+  ###Inputs: 
+  ###Org=c("Mouse",Human","Rat")
+  ###Onto=c("MF",BP","CC")
+  ###fNames = feature names in the dataset (Entrez IDs).
+  ###nMin = minimum number of genes in geneset.
+  ###nMax = maximum number of genes in geneset.
+  switch(Org,
+      Mouse = {require(org.Mm.eg.db)
+        go2entrez <- as.list(org.Mm.egGO2ALLEGS)},
+      Human = {require(org.Mm.eg.db)
+        go2entrez <- as.list(org.Mm.egGO2ALLEGS)},
+      Rat = {require(org.Rn.eg.db)
+        go2entrez <- as.list(org.Rn.egGO2ALLEGS)}
+  )
   
   ## create first input object with GO info
   allGOontol <- eapply(GOTERM, Ontology)  
@@ -39,7 +41,7 @@ f.GOAnnotation <- function(Org="Mouse", Onto="BP",fNames=fNames,nMin=5,nMax=100)
   ###Filter for GO terms related to specific Ontology:
   GOs <- names(which(allGOontol == Onto))
   go <- go2entrez[names(go2entrez) %in% GOs] 
- 
+  
   ###Filter for GO terms related to specific Ontology and specific Chip:
   go.eSet <- vector("list", length(go))
   for (j in 1:length(go)){go.eSet[[j]] <- unique(go[[j]][go[[j]] %in% fNames]  )}
@@ -48,21 +50,21 @@ f.GOAnnotation <- function(Org="Mouse", Onto="BP",fNames=fNames,nMin=5,nMax=100)
   go.eSet <- go.eSet[ind0==T]
   ind1 <- ifelse(lapply(go.eSet,length) >= nMin & lapply(go.eSet,length) <= nMax, TRUE, FALSE)
   go.eSet <- go.eSet[ind1==T]
-
-return(go.eSet)
-
+  
+  return(go.eSet)
+  
 }
 
 
 f.GOInputMLP <- function(x=go.eSet){
-
-###Authors: An De Bondt, Tine Casneuf, Nandini Raghavan.
-###This code converts the output of f.GOAnnotation to the necessary format for input for MLP analysis.
-
-###Input: go.eSet output from f.GOAnnotation.
-
-
-   out <- lapply(names(go.eSet), function(goid) {
+  
+  ###Authors: An De Bondt, Tine Casneuf, Nandini Raghavan.
+  ###This code converts the output of f.GOAnnotation to the necessary format for input for MLP analysis.
+  
+  ###Input: go.eSet output from f.GOAnnotation.
+  
+  
+  out <- lapply(names(go.eSet), function(goid) {
         i.genes <- unique(go.eSet[[goid]])
         matrix(c(rep(x = goid, times = length(i.genes)), i.genes), ncol = 2, byrow = FALSE)
       })
@@ -75,16 +77,6 @@ f.GOInputMLP <- function(x=go.eSet){
   out[,2] <- as.numeric(out[,2])
   out <- unique(out)
   out <- as.matrix(out) ###listed as a matrix as needed for ML
-return(out)
-
+  return(out)
+  
 }
-
-###======================================================================================================================
-
-
-
-
-
-
-
-
